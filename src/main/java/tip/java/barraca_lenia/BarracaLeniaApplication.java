d51a0bce2686acae6936681c8b1ed015382790fe
Lucas <lucas.ortiz@estudiantes.utec.edu.uy>
2026-04-27 23:11:15 -0300

    Primer commit - proyecto barraca JAVA

---
+++ src/main/java/tip/java/barraca_lenia/BarracaLeniaApplication.java
@@ -0,0 +1,13 @@
+package tip.java.barraca_lenia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarracaLeniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarracaLeniaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tip/java/barraca_lenia/BarracaLeniaApplicationTests.java
@@ -0,0 +1,13 @@
+package tip.java.barraca_lenia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarracaLeniaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
