a0e829f9dfc669f4f4626929ae12967d94a1ff8c
Lucas <lucas.ortiz@estudiantes.utec.edu.uy>
2026-05-01 18:00:55 -0300

    Arreglos de usuario CRUD

---
+++ src/main/java/tip/java/barraca_lenia/configurations/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package tip.java.barraca_lenia.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/usuarios/**").permitAll()
+                        .requestMatchers("/usuariosListar/**").permitAll()
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
